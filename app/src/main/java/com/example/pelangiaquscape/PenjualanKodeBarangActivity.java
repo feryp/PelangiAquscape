832dab3f0a1aced79ab98a3a267df582ba98fdad
Mona Lisa <yogadanni0@gmail.com>
2019-07-23 13:00:08 +0700

    // add kode barang feature

--- app/src/main/java/com/example/pelangiaquscape/PenjualanKodeBarangActivity.java
+++ app/src/main/java/com/example/pelangiaquscape/PenjualanKodeBarangActivity.java
@@ -1,13 +1,111 @@
 package com.example.pelangiaquscape;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.pelangiaquscape.Interface.ItemClickListener;
+import com.example.pelangiaquscape.Model.Barang;
+import com.example.pelangiaquscape.Model.Merek;
+import com.example.pelangiaquscape.ViewHolder.BarangViewHolder;
+import com.example.pelangiaquscape.ViewHolder.MerekViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class PenjualanKodeBarangActivity extends AppCompatActivity {
 
+    FirebaseDatabase fd;
+    DatabaseReference dr;
+    FirebaseRecyclerAdapter adapter;
+    RecyclerView rv;
+
+    RecyclerView.LayoutManager layoutManager;
+    String id;
+
+    Query query;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_penjualan_kode_barang);
+
+        Intent i = getIntent();
+        id = i.getStringExtra("idMerek");
+
+        fd = FirebaseDatabase.getInstance();
+        dr = fd.getReference().child("Barang");
+
+        //load data merek
+        rv = findViewById(R.id.rv_daftar_kode);
+        rv.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        rv.setLayoutManager(layoutManager);
+
+        loadBarang(id);
+
+    }
+
+    private void loadBarang(String ids) {
+
+                query = FirebaseDatabase.getInstance().getReference().child("Barang").orderByChild("merek").equalTo(ids);
+        FirebaseRecyclerOptions<Barang> options =
+                new FirebaseRecyclerOptions.Builder<Barang>().setQuery(query, Barang.class).build();
+
+        Log.i("SNAPSHOT", options.getSnapshots().toString()+" " +ids);
+
+        adapter = new FirebaseRecyclerAdapter<Barang, BarangViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull BarangViewHolder holder, int position, @NonNull Barang model) {
+                holder.tvKode.setText(model.getKode());
+                holder.tvHarga.setText(String.valueOf(model.getHargaJual()));
+//                holder.im_arrow.setImageResource(R.drawable.ic_arrow_black);
+
+                Log.i("INFORMATION", model.getKode()+" "+model.getMerek());
+                final Barang clickItem = model;
+
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(PenjualanKodeBarangActivity.this, position + "", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+
+            @NonNull
+            @Override
+            public BarangViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.list_kode_barang_penjualan, viewGroup, false);
+                Log.i("Kesini", view.toString());
+                return new BarangViewHolder(view);
+            }
+        };
+
+        rv.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
--- app/src/main/java/com/example/pelangiaquscape/ViewHolder/BarangViewHolder.java
+++ app/src/main/java/com/example/pelangiaquscape/ViewHolder/BarangViewHolder.java
@@ -1,14 +1,64 @@
 package com.example.pelangiaquscape.ViewHolder;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class BarangViewHolder extends RecyclerView.ViewHolder{
+import com.example.pelangiaquscape.Interface.ItemClickListener;
+import com.example.pelangiaquscape.R;
+
+public class BarangViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView tvKode;
+    public TextView tvHarga;
+    public TextView tvQuantity;
+    public AppCompatImageButton min;
+    public AppCompatImageButton max;
+
+    int qty = 0;
+
+    private ItemClickListener itemClickListener;
 
 
     public BarangViewHolder(@NonNull View itemView) {
         super(itemView);
 
+        tvKode = itemView.findViewById(R.id.tv_kode);
+        tvHarga = itemView.findViewById(R.id.tv_harga_barang);
+        tvQuantity = itemView.findViewById(R.id.tv_kuantitas);
+        min = itemView.findViewById(R.id.btn_minus);
+        max = itemView.findViewById(R.id.btn_plus);
+        tvQuantity.setText("0");
+        itemView.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v, getAdapterPosition(), false);
+        switch(v.getId()){
+            case R.id.btn_minus:
+                if(qty < 30)
+                    qty = qty+1;
+                tvQuantity.setText(String.valueOf(qty));
+                break;
+            case R.id.btn_plus:
+                if(qty > -1)
+                    qty = qty-1;
+                tvQuantity.setText(String.valueOf(qty));
+                break;
+        }
+    }
+
+    public void setItemClickListener(ItemClickListener i){
+        this.itemClickListener = i;
+    }
+
+
+
+
 }

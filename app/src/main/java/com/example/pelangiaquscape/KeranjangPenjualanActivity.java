0f90214b4c21c385fcde05180524476b1e8bddc5
ferrtama <fery.pratama20@gmail.com>
2019-07-24 10:44:16 +0700

    Update tampilan keranjang

--- app/src/main/java/com/example/pelangiaquscape/KeranjangPenjualanActivity.java
+++ app/src/main/java/com/example/pelangiaquscape/KeranjangPenjualanActivity.java
@@ -3,11 +3,11 @@ package com.example.pelangiaquscape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TransaksiPenjualanActivity extends AppCompatActivity {
+public class KeranjangPenjualanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaksi_penjualan);
+        setContentView(R.layout.activity_keranjang_penjualan);
     }
 }

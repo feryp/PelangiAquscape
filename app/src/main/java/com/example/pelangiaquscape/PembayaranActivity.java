a3078d396e47f978f36045ada9107eba49e4f28d
ferrtama <fery.pratama20@gmail.com>
2019-07-26 22:16:52 +0700

    Update tampilan list kode barang

--- app/src/main/java/com/example/pelangiaquscape/PembayaranActivity.java
+++ app/src/main/java/com/example/pelangiaquscape/PembayaranActivity.java
@@ -1,7 +1,11 @@
 package com.example.pelangiaquscape;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class PembayaranActivity extends AppCompatActivity {
 
@@ -9,6 +13,30 @@ public class PembayaranActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pembayaran);
+
+        final Button buttonUasPas = findViewById(R.id.btn_uang_pas);
+        final Button buttonKelDua = findViewById(R.id.btn_kelipatan_dua);
+        final Button buttonKelLima = findViewById(R.id.btn_kelipatan_lima);
+        final Button buttonKelSepuluh = findViewById(R.id.btn_kelipatan_sepuluh);
+
+        buttonUasPas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonUasPas.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_clickable));
+            }
+        });
+
+//        buttonUasPas.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+//                    buttonUasPas.setBackgroundColor(Color.BLUE);
+//                } else if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+//                    buttonUasPas.setBackgroundColor(Color.GRAY);
+//                }
+//                return false;
+//            }
+//        });
 
 
     }

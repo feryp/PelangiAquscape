2fe9bc499e65de30c4b4e84268fba6a61ff28b19
Mona Lisa <yogadanni0@gmail.com>
2019-08-18 11:27:30 +0700

    // update feature search

--- app/src/main/java/com/example/pelangiaquscape/TransaksiActivity.java
+++ app/src/main/java/com/example/pelangiaquscape/TransaksiActivity.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import com.example.pelangiaquscape.Interface.ItemClickListener;
 import com.example.pelangiaquscape.Model.Merek;
@@ -31,6 +32,7 @@ public class TransaksiActivity extends AppCompatActivity {
     Query query;
     ImageView cancel;
     ImageView keranjang;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,7 @@ public class TransaksiActivity extends AppCompatActivity {
 
         cancel =  findViewById(R.id.im_cancel);
         keranjang = findViewById(R.id.im_keranjang);
+        searchView = findViewById(R.id.search_merek_barang);
 
         //init Firebase
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -49,6 +52,29 @@ public class TransaksiActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                searchFirebase(s);
+                adapter.startListening();
+                return false;
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                loadMerk();
+                adapter.startListening();
+                return false;
+            }
+        });
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,8 +95,23 @@ public class TransaksiActivity extends AppCompatActivity {
 
     }
 
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
     private void loadMerk() {
-        query = FirebaseDatabase.getInstance().getReference().child("Merek");
+        query = FirebaseDatabase.getInstance().getReference().child("Merek").orderByChild("nama");
         FirebaseRecyclerOptions<Merek> options =
                 new FirebaseRecyclerOptions.Builder<Merek>().setQuery(query, Merek.class).build();
 
@@ -110,17 +151,67 @@ public class TransaksiActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        adapter.startListening();
+    private void searchFirebase(String searchText){
+        Log.i("MASUK KE SEARCH ","hehe");
 
-    }
+        query = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("Merek")
+                .orderByChild("nama").startAt(searchText.toUpperCase()).endAt(searchText.toUpperCase() + "\uf8ff");
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        adapter.stopListening();
+//        query = FirebaseDatabase
+//                .getInstance()
+//                .getReference()
+//                .child("Merek")
+//                .orderByChild("nama").startAt(searchText.toUpperCase()).endAt(searchText.toUpperCase());
+
+        FirebaseRecyclerOptions<Merek> options =
+                new FirebaseRecyclerOptions.Builder<Merek>()
+                        .setQuery(query, Merek.class)
+                        .build();
+
+//        Log.i("SnapShot : ",options.getSnapshots().toString());
+        adapter = new FirebaseRecyclerAdapter<Merek, TransaksiMerekViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull TransaksiMerekViewHolder holder, int position, @NonNull final Merek model) {
+                holder.tv_merek.setText(model.getNama());
+//                holder.im_arrow.setImageResource(R.drawable.ic_arrow_black);
+
+                Log.i("INFORMATION", model.getNama()+" "+model.getNama());
+                final Merek clickItem = model;
+
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Intent merek = new Intent(TransaksiActivity.this, TransaksiKodeBarangActivity.class);
+                        merek.putExtra("idMerek", adapter.getRef(position).getKey());
+                        merek.putExtra("namaMerek", model.getNama());
+                        Log.i("GET IDMEREK", merek.getStringExtra("idMerek") + adapter.getRef(position).getKey());
+                        startActivity(merek);
+                    }
+                });
+            }
+
+
+
+
+            @NonNull
+            @Override
+            public TransaksiMerekViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_merk_barang_transaksi, parent, false);
+                Log.i("Buat View Holder", view.toString());
+                return new TransaksiMerekViewHolder(view);
+            }
+
+
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 
 

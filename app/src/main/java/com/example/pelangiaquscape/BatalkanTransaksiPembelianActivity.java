047c4f150aaa709126b32ea9697a80987af3d973
ferrtama <fery.pratama20@gmail.com>
2019-09-10 11:57:35 +0700

    Update proses tampilan detail proses pembelian dan detail selesai pembelian dan list item detail barang

---
+++ app/src/main/java/com/example/pelangiaquscape/BatalkanTransaksiPembelianActivity.java
@@ -0,0 +1,13 @@
+package com.example.pelangiaquscape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BatalkanTransaksiPembelianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batalkan_transaksi_pembelian);
+    }
+}

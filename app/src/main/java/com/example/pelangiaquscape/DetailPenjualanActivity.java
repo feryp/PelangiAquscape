84759bd68971152b9ad0ee6eda92d931455f36e2
ferrtama <fery.pratama20@gmail.com>
2019-09-22 09:32:50 +0700

    update detail penjualan, min merek, diskon, nama kasir, sama harga jual tambahan lihat struk

--- app/src/main/java/com/example/pelangiaquscape/DetailPenjualanActivity.java
+++ app/src/main/java/com/example/pelangiaquscape/DetailPenjualanActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -79,6 +80,14 @@ public class DetailPenjualanActivity extends AppCompatActivity {
 
         DetailPenjualanAdapter adapter = new DetailPenjualanAdapter(listItemPenjualan, this);
         rvDetailItem.setAdapter(adapter);
+
+        btnLihatStruk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lihatStruk = new Intent(DetailPenjualanActivity.this, StrukPenjualanActivity.class );
+                startActivity(lihatStruk);
+            }
+        });
 
     }
 }

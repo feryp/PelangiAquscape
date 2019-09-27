2cea90bbde56509d573d524a0d4b17b46f36b6bd
ferrtama <fery.pratama20@gmail.com>
2019-09-27 14:47:34 +0700

    update tampilan syarat dan ketentuan

--- app/src/main/java/com/example/pelangiaquscape/TentangKamiActivity.java
+++ app/src/main/java/com/example/pelangiaquscape/TentangKamiActivity.java
@@ -1,20 +1,33 @@
 package com.example.pelangiaquscape;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class TentangKamiActivity extends AppCompatActivity {
 
     ImageView cancel;
+
+    LinearLayout syaratKetentuan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tentang_kami);
 
-        cancel =(ImageView) findViewById(R.id.im_cancel);
+        cancel = findViewById(R.id.im_cancel);
+        syaratKetentuan = findViewById(R.id.syarat_dan_ketentuan);
+
+        syaratKetentuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TentangKamiActivity.this, SyaratKetentuanAplikasiActivity.class);
+                startActivity(intent);
+            }
+        });
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override

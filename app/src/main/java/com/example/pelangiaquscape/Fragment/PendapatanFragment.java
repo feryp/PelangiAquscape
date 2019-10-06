7e4af71d9873d3fc1d0e9c24197dc8f5e7c713c4
Mona Lisa <yogadanni0@gmail.com>
2019-10-06 12:24:02 +0700

    // laporan

--- app/src/main/java/com/example/pelangiaquscape/Fragment/PendapatanFragment.java
+++ app/src/main/java/com/example/pelangiaquscape/Fragment/PendapatanFragment.java
@@ -6,117 +6,270 @@ import android.graphics.Paint;
 import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.pelangiaquscape.Model.Penjualan;
 import com.example.pelangiaquscape.R;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LegendEntry;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 
 public class PendapatanFragment extends Fragment {
 
     BarChart barChart;
+    TextView tvTotal;
+    Spinner spinner;
+
+    List<Penjualan> listPenjualan = new ArrayList<>();
+
+    final String[] bulan = {"Januari",
+            "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+    double jan, feb, mar, apr, may, june, jul, aug, sep, oct, nov, dec;
+
+    int[] RAINBOW_COLOR ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v  = inflater.inflate(R.layout.fragment_pendapatan, container, false);
+        View v = inflater.inflate(R.layout.fragment_pendapatan, container, false);
 
 //        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "res/font/roboto_regular.ttf");
 
+        tvTotal = v.findViewById(R.id.total_saldo);
         barChart = v.findViewById(R.id.bar_pendapatan);
+        spinner = v.findViewById(R.id.spinner_tahun_pendapatan);
+
+        RAINBOW_COLOR = getContext().getResources().getIntArray(R.array.rainbow);
 
 
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(10f,0));
-        barEntries.add(new BarEntry(6f,1));
-        barEntries.add(new BarEntry(20f,2));
-        barEntries.add(new BarEntry(30f,3));
-        barEntries.add(new BarEntry(15f,4));
-        barEntries.add(new BarEntry(3f,5));
-        barEntries.add(new BarEntry(20f,6));
-        barEntries.add(new BarEntry(22f,7));
-        barEntries.add(new BarEntry(25f,8));
-        barEntries.add(new BarEntry(12f,9));
-        barEntries.add(new BarEntry(15f,10));
-        barEntries.add(new BarEntry(28f,11));
-        barEntries.add(new BarEntry(31f,12));
 
-
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Pendapatan");
-
-        ArrayList<String> pendapatan = new ArrayList<>();
-        pendapatan.add("Januari");
-        pendapatan.add("Februari");
-        pendapatan.add("Maret");
-        pendapatan.add("April");
-        pendapatan.add("Mei");
-        pendapatan.add("Juni");
-        pendapatan.add("Juli");
-        pendapatan.add("Agustus");
-        pendapatan.add("September");
-        pendapatan.add("Oktober");
-        pendapatan.add("November");
-        pendapatan.add("Desember");
-
-
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setGranularity(1f);
-        xAxis.setGranularityEnabled(true);
-        BarData barData = new BarData(barDataSet);
-        barData.setBarWidth(1f); //set custom bar width
-        barData.setValueTextSize(12);
         Description description = new Description();
         description.setTextColor(R.color.colorBlueSold);
         description.setText("Rekap Kas Pendapatan");
         barChart.setDescription(description);
-        barChart.setData(barData);
-        barChart.setFitBars(true); // make the x-axis fit exactly all bars
-        barChart.invalidate(); //refresh
-        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(pendapatan));
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int year = Integer.parseInt(spinner.getSelectedItem().toString());
+                barChart.clear();
+                loadDataPendapatan(year);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         barChart.setTouchEnabled(true);
         barChart.setDragEnabled(true);
         barChart.setScaleEnabled(true);
 
-        barDataSet.setGradientColor(Color.parseColor("#238BD8"), Color.parseColor("#2346D8"));
-
-
-        Legend legend = barChart.getLegend();
-        legend.setFormSize(10f);
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setTextSize(12f);
-        legend.setTextColor(Color.BLACK);
-//        List<LegendEntry> legendEntries = new ArrayList<>();
-//        for (int i = 0; i < pendapatan.size(); i++){
-//            LegendEntry entry = new LegendEntry();
-//            entry.formColor = Color.parseColor("#238BD8");
-//            entry.label = pendapatan.get(i);
-//            legendEntries.add(entry);
-//
-//        }
-        legend.setXEntrySpace(5f);
-        legend.setYEntrySpace(5f);
-//        legend.setCustom(legendEntries);
+//        loadDataPendapatan();
 
         return v;
 
 
+    }
+
+    void loadDataPendapatan(int year) {
+        FirebaseDatabase.getInstance().getReference("Penjualan").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Calendar c = Calendar.getInstance();
+
+                double total = 0;
+
+                List<BarEntry> entries = new ArrayList<>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    Penjualan penjualan = ds.getValue(Penjualan.class);
+                    c.setTimeInMillis(penjualan.getTanggalPenjualan());
+
+
+                    int month = c.get(Calendar.MONTH);
+                    int year1 = c.get(Calendar.YEAR);
+
+                    if(year == year1){
+                        switch (month) {
+                            case 0:
+                                jan = jan + penjualan.getTotalPenjualan();
+                                break;
+                            case 1:
+                                feb = feb + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 2:
+                                mar = mar + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 3:
+                                apr = apr + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 4:
+                                may = may + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 5:
+                                june = june + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 6:
+                                jul = jul + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 7:
+                                aug = aug + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 8:
+                                sep = sep + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 9:
+                                oct = oct + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 10:
+                                nov = nov + penjualan.getTotalPenjualan();
+
+                                break;
+                            case 11:
+                                dec = dec + penjualan.getTotalPenjualan();
+
+                                break;
+
+                        }
+
+                        total += penjualan.getTotalPenjualan();
+                    }
+
+
+                    tvTotal.setText(String.valueOf(total));
+
+
+
+
+                }
+                entries.add(new BarEntry(0, (float) jan));
+                entries.add(new BarEntry(1, (float) feb));
+                entries.add(new BarEntry(2, (float) mar));
+                entries.add(new BarEntry(3, (float) apr));
+                entries.add(new BarEntry(4, (float) may));
+                entries.add(new BarEntry(5, (float) june));
+                entries.add(new BarEntry(6, (float) jul));
+                entries.add(new BarEntry(7, (float) aug));
+                entries.add(new BarEntry(8, (float) sep));
+                entries.add(new BarEntry(9, (float) oct));
+                entries.add(new BarEntry(10, (float) nov));
+                entries.add(new BarEntry(11, (float) dec));
+
+//                    entries.add(new BarEntry(0, (float) 1000000));
+//                    entries.add(new BarEntry(1, (float) 1000000));
+//                    entries.add(new BarEntry(2, (float) 1000000));
+//                    entries.add(new BarEntry(3, (float) 1000000));
+//                    entries.add(new BarEntry(4, (float) 1000000));
+//                    entries.add(new BarEntry(5, (float) 1000000));
+//                    entries.add(new BarEntry(6, (float) 1000000));
+//                    entries.add(new BarEntry(7, (float) aug));
+//                    entries.add(new BarEntry(8, (float) sep));
+//                    entries.add(new BarEntry(9, (float) 1000000));
+//                    entries.add(new BarEntry(10, (float) 1000000));
+//                    entries.add(new BarEntry(11, (float) 1000000));
+
+                System.out.println("jan " + jan + "\n"
+                        + "feb " + feb + "\n"
+                        + "mar " + mar + "\n"
+                        + "apr " + apr + "\n"
+                        + "may " + may + "\n"
+                        + "june  " + june + "\n"
+                        + "jul " + jul + "\n"
+                        + "aug " + aug + "\n"
+                        + "sep " + sep + "\n"
+                        + "oct " + oct + "\n"
+                        + "nov " + nov + "\n"
+                        + "dec " + dec + "\n"
+                );
+
+//                    List<BarDataSet> listDataSet = new ArrayList<>();
+//                    for(BarEntry entry:entries){
+//                        String bln = bulan[entries.indexOf(entry)];
+//                        listDataSet.add(new BarDataSet(entry, bln));
+//                    }
+                BarDataSet dataSet = new BarDataSet(entries, "Labels");
+                dataSet.setColors(RAINBOW_COLOR);
+
+                XAxis xAxis = barChart.getXAxis();
+
+                xAxis.setGranularity(1f);
+                xAxis.setGranularityEnabled(true);
+                xAxis.setLabelRotationAngle(90);
+
+                YAxis yAxis = barChart.getAxisRight();
+                yAxis.setDrawLabels(false);
+
+                BarData barData = new BarData(dataSet);
+                barData.setBarWidth(1f); //set custom bar width
+                barData.setValueTextSize(12);
+
+                barChart.setData(barData);
+
+//                Legend legend = barChart.getLegend();
+//                legend.setWordWrapEnabled(true);
+//                legend.setFormSize(10f);
+//                legend.setForm(Legend.LegendForm.SQUARE);
+//                legend.setTextSize(12f);
+//                legend.setTextColor(Color.BLACK);
+//                List<LegendEntry> legendEntries = new ArrayList<>();
+//
+//                for (int i = 0; i < entries.size(); i++) {
+//                    LegendEntry entry = new LegendEntry();
+//                    entry.formColor = RAINBOW_COLOR[i];
+//                    entry.label = bulan[i];
+//                    legendEntries.add(entry);
+//
+//                }
+//                legend.setXEntrySpace(5f);
+//                legend.setYEntrySpace(5f);
+//                legend.setCustom(legendEntries);
+                barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(bulan));
+                barChart.invalidate();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }

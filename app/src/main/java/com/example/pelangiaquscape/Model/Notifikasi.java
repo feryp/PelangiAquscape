89509ee46216350dd17e76f851511ef73961a9a0
ferrtama <fery.pratama20@gmail.com>
2019-11-29 19:38:47 +0700

    update on/off notif

---
+++ app/src/main/java/com/example/pelangiaquscape/Model/Notifikasi.java
@@ -0,0 +1,21 @@
+package com.example.pelangiaquscape.Model;
+
+public class Notifikasi {
+    private boolean notifikasi;
+
+
+
+    public Notifikasi(boolean notifikasi) {
+        this.notifikasi = notifikasi;
+    }
+
+    public Notifikasi(){}
+
+    public boolean isNotifikasi() {
+        return notifikasi;
+    }
+
+    public void setNotifikasi(boolean notifikasi) {
+        this.notifikasi = notifikasi;
+    }
+}
--- app/src/main/java/com/example/pelangiaquscape/NotifikasiActivity.java
+++ app/src/main/java/com/example/pelangiaquscape/NotifikasiActivity.java
@@ -5,7 +5,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,19 +17,23 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.pelangiaquscape.Model.Notifikasi;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import static com.example.pelangiaquscape.Notification.NotificationHelper.CHANNEL_ID;
 
 public class NotifikasiActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    public static final String mypreferences = "mypref";
     final String EXTRA = "INTENT_EDIT_TO_MAIN";
     ImageView cancel;
     SwitchCompat notifikasi;
     NotificationManager notificationManager;
     FirebaseDatabase db;
     DatabaseReference dr;
-
+    SharedPreferences sharedPreferences;
+    String PACKAGE_NAME = "com.example.pelangiaquascape.";
+    private boolean fromNotifikasi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,68 +51,43 @@ public class NotifikasiActivity extends AppCompatActivity implements CompoundBut
             }
         });
 
-        notifikasi.setOnCheckedChangeListener(this);
+        if (notifikasi != null){
+            notifikasi.setOnCheckedChangeListener(this);
+        }
+
 
 
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked){
-            Toast.makeText(NotifikasiActivity.this,"Notifikasi Nyala", Toast.LENGTH_SHORT).show();
+        Toast.makeText(NotifikasiActivity.this, "Notifikasi switch " + (isChecked ? "on" : "off"),
+                Toast.LENGTH_SHORT).show();
+        if (isChecked) {
+            Toast.makeText(NotifikasiActivity.this, "Notifikasi Nyala", Toast.LENGTH_SHORT).show();
             notifikasi.setChecked(true);
+            saveNotifSetting(true);
         } else {
-            Toast.makeText(NotifikasiActivity.this,"Notifikasi Mati", Toast.LENGTH_SHORT).show();
+            Toast.makeText(NotifikasiActivity.this, "Notifikasi Mati", Toast.LENGTH_SHORT).show();
             notifikasi.setChecked(false);
+            saveNotifSetting(false);
         }
 
     }
 
+    public void saveNotifSetting(Boolean notifikasi) {
 
-//    void showNotification(){
-//        int icon = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? R.drawable.logoblack : R.drawable.logoblack;
-//
-//        String message = "Stok minimal "+ barang.getMinStok() + " ,Stok saat ini "+barang.getStok();
-//        String title = "Barang " + barang.getKode() + " telah mencapai stok minimal";
-//
-//        Intent intent = new Intent(this, DetailPemberitahuanActivity.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//
-//        intent.putExtra("message", message);
-//        intent.putExtra("title", title);
-//        intent.putExtra("tanggal",date);
-//
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-//                PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-//                .setSmallIcon(icon)
-//                .setContentTitle(title)
-//                .setContentText(message)
-//                .setAutoCancel(true)
-//                .setContentIntent(pendingIntent)
-//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//
-//
-//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-//            Notification notification = new Notification.Builder(this, CHANNEL_ID)
-//                    .setContentTitle(title)
-//                    .setContentText(message)
-//                    .setSmallIcon(icon)
-//                    .setAutoCancel(true)
-//                    .setContentIntent(pendingIntent)
-//                    .build();
-//
-//            notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//            // notificationId is a unique int for each notification that you must define
-//            notificationManager.notify(1221, notification);
-//
-//        }
-//
-//        // notificationId is a unique int for each notification that you must define
-//        notificationManager.notify(1221, builder.build());
-//    }
+        sharedPreferences = getApplicationContext().getSharedPreferences(PACKAGE_NAME + "SETTING_NOTIF", Context.MODE_PRIVATE);
 
-}
+        sharedPreferences.getBoolean("notif", false);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("notif", notifikasi);
+            editor.apply();
+//            Intent intent = new Intent(NotifikasiActivity.this, PembayaranActivity.class);
+//            startActivity(intent);
+        }
+
+
+    }
+
+
